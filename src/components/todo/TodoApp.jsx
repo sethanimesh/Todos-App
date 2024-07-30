cf7f9ba1c9d24795687fc56e5ce98ca5cb83814b
animesh <sethanimesh@hotmail.com>
2024-07-30 10:05:00 +0530

    login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,28 @@
 export default function TodoApp(){
     return (
-        <div>Todo Application</div>
+        <>
+            <div>Todo Application</div>
+            <LoginComponent/>
+        </>
+    )
+}
+
+function LoginComponent(){
+    return (
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username"/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password"/>
+                </div>
+                <div>
+                    <button type="button" name="login">Login</button>
+                </div>
+            </div>
+        </div>
     )
 }
