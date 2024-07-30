b87f08e8bbf756311fd69c2c2f05590ead50507c
animesh <sethanimesh@hotmail.com>
2024-07-30 11:20:01 +0530

    hardcoded authentication

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -14,6 +14,9 @@ function LoginComponent(){
     const[username, setUsername] = useState('sethanimesh')
     const[password, setPassword] = useState('')
 
+    const[showSuccesssMessage, setSuccessMessage] = useState(false)
+    const[showErrorMessage, setErrorMessage] = useState(false)
+
     function handleUsernameChange(event){
         setUsername(event.target.value)
     }
@@ -23,10 +26,41 @@ function LoginComponent(){
         setPassword(event.target.value)
     }
 
+    function handleSubmit(){
+        if ((username==="sethanimesh") && (password==="dummy")){
+            setSuccessMessage(true)
+            setErrorMessage(false)
+        }
+        else{
+            setSuccessMessage(false)
+            setErrorMessage(true)
+        }
+    }
+
+    function SuccessMessageComponent(){
+        if (showSuccesssMessage){
+            return (
+                <div className='successMessage'>Authenticated Successfully</div>
+            )
+        }
+        return null
+    }
+
+    function ErrorMessageComponent(){
+        if (showErrorMessage){
+            return (
+                <div className="errorMessage">Authentication Failed</div>
+            )
+        }
+        return null
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
+                    <SuccessMessageComponent/>
+                    <ErrorMessageComponent/>
                     <label>Username</label>
                     <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
@@ -35,7 +69,7 @@ function LoginComponent(){
                     <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
