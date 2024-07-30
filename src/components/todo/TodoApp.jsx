657c66c477ef6d232ae3dcd83bc063958aad6925
animesh <sethanimesh@hotmail.com>
2024-07-30 10:31:53 +0530

    making login as controlled component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,23 +1,38 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
 export default function TodoApp(){
     return (
         <>
-            <div>Todo Application</div>
             <LoginComponent/>
         </>
     )
 }
 
 function LoginComponent(){
+    
+    const[username, setUsername] = useState('sethanimesh')
+    const[password, setPassword] = useState('')
+
+    function handleUsernameChange(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event){
+        console.log(event.target.value)
+        setPassword(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>Username</label>
-                    <input type="text" name="username"/>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="login">Login</button>
@@ -26,3 +41,4 @@ function LoginComponent(){
         </div>
     )
 }
+
