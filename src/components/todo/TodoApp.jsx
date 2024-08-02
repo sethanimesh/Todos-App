9db2ee48f6a7cea6478207ffba85e97a25acacbd
animesh <sethanimesh@hotmail.com>
2024-08-02 18:48:44 +0530

    list of todos

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,6 +10,7 @@ export default function TodoApp(){
                     <Route path="/" element={<LoginComponent/>}></Route>
                     <Route path="/login" element={<LoginComponent/>}></Route>
                     <Route path="/welcome/:username" element={<WelcomeComponent/>}></Route>
+                    <Route path="/listtodos" element={<ListTodosComponent/>}/>
                     <Route path="*" element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -104,6 +105,50 @@ function ErrorComponent(){
             <h1>We are working really hard !</h1>
             <div>
                 Apologies for the 404 Error.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent(){
+
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDate())
+
+    const todos = [
+        {id:1, description:"Learn AWS", isdone:false, targetDate:targetDate},
+        {id:2, description:"Learn DevOps", isdone:false, targetDate:targetDate},
+        {id:3, description:"Learn Full Stack Ops", isdone:false, targetDate:targetDate}
+    ]
+
+    return (
+        <div className='ListTodosComponent'>
+            <h1>Things to do</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DESCRIPTION</th>
+                            <th>IS DONE?</th>
+                            <th>TARGET DATW</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        todos.map(
+                            todo=>(
+                                <tr key={todo.id}> 
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.isdone.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
